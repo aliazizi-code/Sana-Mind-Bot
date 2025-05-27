6c8d1ec0a45b37f98e8c4a689fc2958ed294b12d
aliazizi-code <devaliazizi@gmail.com>
2025-05-27 13:26:42 +0330

    Add Telegram bot app with initial configuration and files

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # My apps
+    'telegram_bot.apps.TelegramBotConfig',
 ]
 
 MIDDLEWARE = [
