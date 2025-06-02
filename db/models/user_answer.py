c912ee7c5de80eb8b720fee02be2f0400b851c18
aliazizi-code <devaliazizi@gmail.com>
2025-06-02 11:48:34 +0330

    Add UserAnswer model with relationships and timestamp for answer submission

--- db/models/user_answer.py
+++ db/models/user_answer.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, ForeignKey
-from sqlalchemy.orm import validates
+from sqlalchemy import Column, Integer, ForeignKey, DateTime
+from utils.time_zone import get_iran_time
+from sqlalchemy.orm import validates, relationship
 from db.base import Base
 
 
@@ -7,10 +8,16 @@ class UserAnswer(Base):
     __tablename__ = 'user_answers'
     
     id = Column(Integer, primary_key=True, autoincrement=True)
+    progress_id = Column(Integer, ForeignKey("user_progress.id"), nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     question_id = Column(Integer, ForeignKey('questions.id'), nullable=False)
     selected_option_id = Column(Integer, ForeignKey('options.id'), nullable=False)
     rank = Column(Integer, nullable=True)
+    answer_at = Column(DateTime, default=lambda: get_iran_time())
+    
+    progress = relationship("UserProgress", back_populates='answers')
+    question = relationship("Question")
+    option = relationship("Option")
     
     @validates('rank')
     def validate_rank(self, key, rank):
