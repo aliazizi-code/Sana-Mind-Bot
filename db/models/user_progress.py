7b401ade9403af4886c41975b784af6b76b7d1f2
aliazizi-code <devaliazizi@gmail.com>
2025-06-02 11:48:14 +0330

    Add UserProgress model to track user test progress and answers

---
+++ db/models/user_progress.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from db.base import Base
+from utils.time_zone import get_iran_time
+
+
+class UserProgress(Base):
+    __tablename__ = 'user_progress'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, nullable=False, index=True)
+    test_id = Column(Integer, ForeignKey("tests.id"), nullable=False)
+    current_order = Column(Integer, default=1)
+    last_taken_date = Column(DateTime, default=lambda: get_iran_time())
+    
+    test = relationship("Test")
+    answer = relationship(
+        "UserAnswer",
+        back_populates='progress',
+        cascade='all, delete-orphan'
+    )
