3d40cab3e1dfc65ed42f6003ec7c85204e36defb
aliazizi-code <devaliazizi@gmail.com>
2025-06-03 18:10:59 +0330

    Add ForeignKey constraint to user_id in UserProgress model

--- db/models/user_progress.py
+++ db/models/user_progress.py
@@ -8,7 +8,7 @@ class UserProgress(Base):
     __tablename__ = 'user_progress'
     
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, nullable=False, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
     test_id = Column(Integer, ForeignKey("tests.id"), nullable=False)
     current_order = Column(Integer, default=1)
     last_taken_date = Column(DateTime, default=lambda: get_iran_time())
