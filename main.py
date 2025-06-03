70bd61a7951a6f9477a288706f4f11c8ec3cd605
aliazizi-code <devaliazizi@gmail.com>
2025-06-03 18:11:03 +0330

    Refactor main.py to import handlers from registry and remove direct CommandHandler for start

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import os
+import logging
 from dotenv import load_dotenv
-from telegram.ext import ApplicationBuilder, CommandHandler
-from bot.handlers.start import start
+from telegram.ext import ApplicationBuilder
+from bot.registry import handlers
+
+
+# logging.basicConfig(
+#     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+# )
+# logger = logging.getLogger(__name__)
 
 load_dotenv()
 
 def main():
     app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
     
-    app.add_handler(CommandHandler("start", start))
+    app.add_handlers(handlers)
     
     app.run_polling()
 
