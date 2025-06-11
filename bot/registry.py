d3ac3b44bf6a8277659d841c32e1c77f277b61e7
aliazizi-code <devaliazizi@gmail.com>
2025-06-11 11:58:57 +0330

    Refactor handler imports in registry.py for improved organization and clarity

--- bot/registry.py
+++ bot/registry.py
@@ -1,20 +1,34 @@
-from bot.handlers import (
-    start,
-    test_list, show_test, start_test, handle_answer, delete_test_progress, show_result
-
+import logging
+from bot.handlers.start import start
+from bot.handlers.manager import create_test_handler
+from bot.handlers.test import (
+    show_result,
+    show_test,
+    delete_test_progress,
+    start_test,
+    handle_answer,
 )
+from bot.utils.test_list_func import test_list
 from telegram.ext import CommandHandler, CallbackQueryHandler
+
+
+logger = logging.getLogger(__name__)
+logger.info("Registering command and callback query handlers...")
 
 
 handlers = [
     # Command Handlers
     CommandHandler("start", start),
     CommandHandler("test", test_list),
+    create_test_handler,
+    
     
     # Callback Query Handlers
     CallbackQueryHandler(show_test, pattern=r"^show_test:\d+$"),
     CallbackQueryHandler(delete_test_progress, pattern=r"^delete_test:\d+$"),
     CallbackQueryHandler(show_result, pattern=r"^show_result:\d+$"),
     CallbackQueryHandler(start_test, pattern=r"^start_test:\d+$"),
-    CallbackQueryHandler(handle_answer, pattern=r"^answer:\d+:\d+:\d+$"),
+    CallbackQueryHandler(handle_answer, pattern=r"^answer:\d+:\d+:\d+$"),  
 ]
+
+logger.info("Handlers registered successfully.")
