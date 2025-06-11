1e670224f2a3f550b8f7cc125df2d1c870f8825b
aliazizi-code <devaliazizi@gmail.com>
2025-06-11 11:42:35 +0330

    Refactor logging setup in main.py for improved clarity and consistency

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ from telegram.ext import ApplicationBuilder
 from bot.registry import handlers
 
 
-# logging.basicConfig(
-#     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-# )
-# logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
 def main():
+    logger.info("Starting the bot...")
     app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
     
     app.add_handlers(handlers)
     
+    logger.info("Bot is polling.")
     app.run_polling()
 
 
