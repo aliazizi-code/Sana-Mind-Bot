106cb1c8e422a22da7ae521592f6dddf4596705d
aliazizi-code <devaliazizi@gmail.com>
2025-06-11 11:42:29 +0330

    Enhance logging in create_db.py for better error tracking and status updates

--- create_db.py
+++ create_db.py
@@ -1,14 +1,22 @@
+import logging
 from db.session import engine
 from db.base import Base
 from db.models import *
 
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
 def create_table():
+    logging.info("Creating tables in the database...")
     print("Creating tables in the database...")
     
-    Base.metadata.create_all(bind=engine)
-    print("All tables created successfully.")
-    
+    try:
+        Base.metadata.create_all(bind=engine)
+        logging.info("All tables created successfully.")
+        print("All tables created successfully.")
+    except Exception as e:
+        logging.error(f"Error creating tables: {e}")
+        print(f"Error creating tables: {e}")
     
 if __name__ == "__main__":
     create_table()
