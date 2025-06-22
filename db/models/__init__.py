b1d30b5d7a964905f32956ac57a0d9973eb145b5
aliazizi-code <devaliazizi@gmail.com>
2025-06-22 10:47:56 +0330

    Remove unused import of TestType from __init__.py

--- db/models/__init__.py
+++ db/models/__init__.py
@@ -1,8 +1,7 @@
 from .user import User
 from .option import Option
 from .question import Question
-from .test import Test, TestType
+from .test import Test
 from .user_answer import UserAnswer
 from .user_progress import UserProgress
 from .user_result import UserResult
-from .message import Message
