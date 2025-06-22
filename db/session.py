47cd83497c9a830da6919701375da1d74b100133
aliazizi-code <devaliazizi@gmail.com>
2025-06-22 12:30:15 +0330

    Remove unused import of os in session.py

--- db/session.py
+++ db/session.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
-import os
 
 # Load environment variables from .env file
 load_dotenv()
