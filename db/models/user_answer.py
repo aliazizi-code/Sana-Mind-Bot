011f3cdc2d5b20f996701a32df31577446f3ee41
aliazizi-code <devaliazizi@gmail.com>
2025-06-22 10:48:12 +0330

    Refactor UserAnswer model by updating import paths and removing unused rank attribute and validation

--- db/models/user_answer.py
+++ db/models/user_answer.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, ForeignKey, DateTime
-from utils.time_zone import get_iran_time
-from sqlalchemy.orm import validates, relationship
+from bot.utils.time_zone import get_iran_time
+from sqlalchemy.orm import relationship
 from db.base import Base
 
 
@@ -12,16 +12,9 @@ class UserAnswer(Base):
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     question_id = Column(Integer, ForeignKey('questions.id'), nullable=False)
     selected_option_id = Column(Integer, ForeignKey('options.id'), nullable=False)
-    rank = Column(Integer, nullable=True)
     answer_at = Column(DateTime, default=lambda: get_iran_time())
     
     progress = relationship("UserProgress", back_populates='answers')
     question = relationship("Question")
     option = relationship("Option")
-    
-    @validates('rank')
-    def validate_rank(self, key, rank):
-        if rank is not None and rank not in {1, 2}:
-            raise ValueError("Rank must be either 1 or 2")
-        return rank
     
