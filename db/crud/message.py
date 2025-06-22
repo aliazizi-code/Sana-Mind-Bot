6f4883d7597e9db0546e7f504f9f46d56d50d6b4
aliazizi-code <devaliazizi@gmail.com>
2025-06-22 10:47:31 +0330

    Remove unused get_message_by_key function from message.py

--- db/crud/message.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import Session
-from db.models.message import Message
-
-def get_message_by_key(session: Session, key: str, default: str) -> str:
-    msg = session.query(Message).filter_by(key=key).first() 
-    return msg.value if msg else default
