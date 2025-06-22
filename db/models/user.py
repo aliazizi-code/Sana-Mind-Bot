6bb3b12d0fa5d9c70c6bfa6cd00422fd5eed228d
aliazizi-code <devaliazizi@gmail.com>
2025-06-22 10:48:21 +0330

    Refactor User model by updating telegram_id type to BigInteger and fixing import path for get_iran_time

--- db/models/user.py
+++ db/models/user.py
@@ -1,17 +1,15 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, BigInteger
 from db.base import Base
-from utils.time_zone import get_iran_time
+from bot.utils.time_zone import get_iran_time
 
 
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    telegram_id = Column(Integer, unique=True, nullable=False, index=True)
+    telegram_id = Column(BigInteger, unique=True, nullable=False, index=True)
     full_name = Column(String, nullable=False)
     is_admin = Column(Boolean, default=False, nullable=False, index=True)
-    is_active = Column(Boolean, default=True, nullable=False, index=True)
-    is_banned = Column(Boolean, default=False, nullable=False, index=True)
     created_at = Column(DateTime, default=lambda: get_iran_time(), nullable=False)
     
     def __repr__(self):
@@ -21,6 +19,4 @@ class User(Base):
             f"full_name='{self.full_name}', "
             f"username='{self.username}', "
             f"is_admin={self.is_admin}, "
-            f"is_active={self.is_active}, "
-            f"is_banned={self.is_banned})> "
         )
